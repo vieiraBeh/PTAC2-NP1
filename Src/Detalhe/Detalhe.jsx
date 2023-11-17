bddcf004ccad51d454bbecb3540d5de91bf14d64
Brenda Vieira <brendanascimentovieirah@gmail.com>
2023-11-17 13:39:01 -0400

    commit

--- Src/Detalhe/Detalhe.jsx
+++ Src/Detalhe/Detalhe.jsx
@@ -2,6 +2,8 @@ import { useParams } from "react-router-dom";
 
 export default function Detalhe(){
     const { id } = useParams();
+    const lista = JSON.parse( localStorage.getItem("Lista"));
+    
     
     return(
     <div>
